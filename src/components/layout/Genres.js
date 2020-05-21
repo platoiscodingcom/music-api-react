98e207f8bc2c8fc3e40941cb444ae25b5b557332
Maria Hildebrandt <hildebrandt.maria@gmail.com>
2020-05-21 23:24:51 +0200

    implemented genre search

--- src/components/layout/Genres.js
+++ src/components/layout/Genres.js
@@ -1,36 +1,88 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react'
+import { Context } from '../../context'
+import axios from 'axios'
 
 const Genres = () => {
-	return (
-		<section className="section-a section-gray">
-			<div className="container container-icons animated fadeIn delay-1s">
-				<div className="genre-icon">
-					<i className="fas fa-th" />
-					<span className="genre-title">Top 10</span>
-				</div>
-				<div className="genre-icon">
-					<i className="fas fa-headphones-alt" />
-					<span className="genre-title">Metal</span>
-				</div>
-				<div className="genre-icon">
-					<i className="fas fa-guitar" />
-					<span className="genre-title">Rock</span>
-				</div>
-				<div className="genre-icon">
-					<i className="fas fa-compact-disc" />
-					<span className="genre-title">classic</span>
-				</div>
-				<div className="genre-icon">
-					<i className="fas fa-drum" />
-					<span className="genre-title">Jazz</span>
-				</div>
-				<div className="genre-icon">
-					<i className="fas fa-music" />
-					<span className="genre-title">Pop</span>
-				</div>
-			</div>
-		</section>
-	);
-};
+  const [state, setState] = useContext(Context)
+	const [genreId, setGenreId] = useState('')
 
-export default Genres;
+  useEffect(() => {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?f_music_genre_id=${genreId}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}`
+      )
+      .then(res => {
+        let track_list = res.data.message.body.track_list
+        setState({ track_list: track_list })
+      })
+      .catch(err => console.log(err))
+  }, [genreId, setGenreId])
+
+  return (
+    <section className='section-a section-gray'>
+      <div className='container container-icons animated fadeIn delay-1s'>
+        <div className='genre-icon'>
+          <i className='fas fa-th' />
+          <span className='genre-title'>Top 10</span>
+        </div>
+        <div
+          onClick={e => {
+            e.preventDefault()
+            setGenreId(1149)
+          }}
+          style={{ cursor: 'pointer' }}
+          className='genre-icon'
+        >
+          <i className='fas fa-headphones-alt' />
+          <span className='genre-title'>Metal</span>
+        </div>
+        <div
+          className='genre-icon'
+          onClick={e => {
+            e.preventDefault()
+            setGenreId(1150)
+          }}
+          style={{ cursor: 'pointer' }}
+        >
+          <i className='fas fa-guitar' />
+          <span className='genre-title'>Rock</span>
+        </div>
+        <div
+          className='genre-icon'
+          onClick={e => {
+            e.preventDefault()
+            setGenreId(1027)
+          }}
+          style={{ cursor: 'pointer' }}
+        >
+          <i className='fas fa-compact-disc' />
+          <span className='genre-title'>classic</span>
+        </div>
+        <div
+          className='genre-icon'
+          onClick={e => {
+            e.preventDefault()
+            setGenreId(1107)
+          }}
+          style={{ cursor: 'pointer' }}
+        >
+          <i className='fas fa-drum' />
+          <span className='genre-title'>Jazz</span>
+        </div>
+        <div
+          className='genre-icon'
+          onClick={e => {
+            e.preventDefault()
+            setGenreId(1133)
+          }}
+          style={{ cursor: 'pointer' }}
+        >
+          <i className='fas fa-music' />
+          <span className='genre-title'>Pop</span>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Genres
--- src/components/tracks/Search.js
+++ src/components/tracks/Search.js
@@ -1,62 +1,60 @@
-import React, { useState, useEffect, useContext } from "react";
-import axios from "axios";
-import { Context } from "../../context";
+import React, { useState, useEffect, useContext } from 'react'
+import axios from 'axios'
+import { Context } from '../../context'
 
 const Search = () => {
-  const [state, setState] = useContext(Context);
-  const [userInput, setUserInput] = useState("");
-  const [trackTitle, setTrackTitle] = useState("");
+  const [state, setState] = useContext(Context)
+  const [userInput, setUserInput] = useState('')
+  const [trackTitle, setTrackTitle] = useState('')
 
-
-  useEffect( () => {
+  useEffect(() => {
     axios
       .get(
-        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track=${trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${
-          process.env.REACT_APP_MM_KEY
-        }`
-      ).then(res => {
-          let track_list = res.data.message.body.track_list;
-          setState({ track_list: track_list });
-          setUserInput("")
-        })
-        .catch(err => console.log(err));
-  }, [trackTitle]);  //only fetch if trackTitle changes
+        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track=${trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}`
+      )
+      .then(res => {
+        let track_list = res.data.message.body.track_list
+        setState({ track_list: track_list })
+        setUserInput('')
+      })
+      .catch(err => console.log(err))
+  }, [trackTitle]) //only fetch if trackTitle changes
 
   const findTrack = e => {
-    e.preventDefault();
-    setTrackTitle(userInput);
+    e.preventDefault()
+    setTrackTitle(userInput)
     window.scrollTo({
       top: 350,
       left: 0,
       behavior: 'smooth'
-    });
-  };
+    })
+  }
 
   const onChange = e => {
-    setUserInput(e.target.value);
-  };
+    setUserInput(e.target.value)
+  }
 
-  return(
-    <div className="header">
-      <div className="input-icon-wrap animated fadeIn">
-        <span className="input-icon">
-          <i className="fas fa-search"></i>
+  return (
+    <div className='header'>
+      <div className='input-icon-wrap animated fadeIn'>
+        <span className='input-icon'>
+          <i className='fas fa-search'></i>
         </span>
-        <form onSubmit={findTrack} className="search-form">
+        <form onSubmit={findTrack} className='search-form'>
           <input
-            type="text"
-            className="input-with-icon"
-            id="form-name"
-            name="userInput"
-            autoComplete="off"
-            placeholder="search for music, albums, artists ..."
+            type='text'
+            className='input-with-icon'
+            id='form-name'
+            name='userInput'
+            autoComplete='off'
+            placeholder='search for music, albums, artists ...'
             value={userInput}
             onChange={onChange}
           />
         </form>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Search;
+export default Search
